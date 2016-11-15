c8d267fda62ec8cd09ba192108120c53fadcee0a
ideahitme <yerken.tussupbekov@zalando.de>
2016-11-15 17:11:22 +0100

    introduce endpoint converter + tests

--- pkg/endpoint.go
+++ pkg/endpoint.go
@@ -1,5 +1,17 @@
 package pkg
 
+import (
+	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/aws/aws-sdk-go/aws"
+	"strings"
+	"fmt"
+)
+
+var (
+	EvaluateTargetHealth = true
+	clusterName = "dummy" //this needs to be replaced by quering for cluster name in kubernetes
+)
+
 // Endpoint is used to pass data from the producer to the consumer.
 type Endpoint struct {
 
@@ -14,3 +26,40 @@ type Endpoint struct {
 	// the service.
 	Hostname string
 }
+
+//FQDN ...
+//return the fully qualified domain name with trailing dot
+func FQDN(dns string) string{
+	return strings.Trim(dns, ".") + ".";
+}
+
+//AWSARecordAlias ...
+//create an AWS A Alias record
+func (ep *Endpoint) AWSARecordAlias(zoneID *string, ttl int64) *route53.ResourceRecordSet {
+	rs := &route53.ResourceRecordSet{
+		Type: aws.String("A"),
+		Name: aws.String(ep.DNSName),
+		TTL: &ttl,
+		AliasTarget: &route53.AliasTarget{
+			DNSName: &ep.Hostname,
+			EvaluateTargetHealth: &EvaluateTargetHealth,
+			HostedZoneId: zoneID,
+		},
+	}
+	return rs
+}
+
+//AWSTXTRecord ...
+//create a AWS TXT record
+func (ep *Endpoint) AWSTXTRecord(ttl int64) *route53.ResourceRecordSet{
+	rs := &route53.ResourceRecordSet{
+		Type: aws.String("TXT"),
+		Name: aws.String(ep.DNSName),
+		TTL: &ttl,
+		ResourceRecords: []*route53.ResourceRecord{{
+			Value: aws.String(fmt.Sprintf("\"mate:%s\"", clusterName)),
+		}},
+	}
+	return rs
+}
+
---
+++ pkg/endpoint_test.go
@@ -0,0 +1,52 @@
+package pkg
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+  zoneID = "ABCDEFG"
+  ttl = int64(300)
+)
+
+func Test_AWSARecordAlias(t *testing.T){
+  ep := &Endpoint{
+    DNSName: "example.com",
+    IP: "10.202.10.123",
+    Hostname: "amazon.elb.com",
+  }
+  rsA := ep.AWSARecordAlias(&zoneID, ttl)
+  assert.Equal(t, *rsA.Type, "A", "Create an A Record")
+}
+
+func Test_AWSTXTRecord(t *testing.T){
+  ep := &Endpoint{
+    DNSName: "example.com",
+    IP: "10.202.10.123",
+    Hostname: "amazon.elb.com",
+  }
+  rsTXT := ep.AWSTXTRecord(ttl)
+  assert.Equal(t, *rsTXT.Type, "TXT", "Create TXT Record")  
+  assert.Equal(t, *rsTXT.Name, "example.com", "Name should match")
+  assert.Equal(t, len(rsTXT.ResourceRecords), 1, "Should include one resource record")
+  assert.Equal(t, *rsTXT.ResourceRecords[0].Value, "\"mate:" + clusterName + "\"")
+}
+
+func TestFQDN(t *testing.T){
+  dns1 := ""
+  res1 := FQDN(dns1)
+  if res1 != "." {
+    t.Errorf("FQDN failed for %s", dns1)
+  }
+  dns2 := "example.com"
+  res2 := FQDN(dns2)
+  if res2 != "example.com." {
+    t.Errorf("FQDN failed for %s", dns2)
+  }
+  dns3 := "example.com."
+  res3 := FQDN(dns3)
+  if res3 != "example.com." {
+    t.Errorf("FQDN failed for %s", dns3)
+  }
+}
