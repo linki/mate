0644e8877332310a07a075f5b71cb24174fb7eb8
ideahitme <yerken.tussupbekov@zalando.de>
2016-11-18 17:33:38 +0100

    aws test fix to pass with new aws client interface

--- awsclient/awsclienttest/client.go
+++ awsclient/awsclienttest/client.go
@@ -2,19 +2,29 @@ package awsclienttest
 
 import (
 	"errors"
+	"fmt"
 
 	"github.bus.zalan.do/teapot/mate/pkg"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/route53"
 )
+
+type Options struct {
+	HostedZone   string
+	RecordSetTTL int
+	GroupID      string
+}
 
 type Client struct {
 	Records      map[string]string
 	AliasRecords map[string]string
-	LastUpsert   []*pkg.Endpoint
-	LastDelete   []*pkg.Endpoint
+	LastUpsert   []*route53.ResourceRecordSet
+	LastDelete   []*route53.ResourceRecordSet
 	failNext     error
+	Options      Options
 }
 
-func (c *Client) ListRecordSets() ([]*pkg.Endpoint, error) {
+func (c *Client) ListRecordSets() ([]*route53.ResourceRecordSet, error) {
 	if err := c.checkFailNext(); err != nil {
 		return nil, err
 	}
@@ -29,37 +39,48 @@ func (c *Client) ListRecordSets() ([]*pkg.Endpoint, error) {
 		records = append(records, &pkg.Endpoint{DNSName: dns, Hostname: hostname})
 	}
 
-	return records, nil
+	rrecords, err := c.MapEndpoints(records)
+	return rrecords, err
 }
 
-func (c *Client) ChangeRecordSets(upsert, del []*pkg.Endpoint) error {
+func (c *Client) MapEndpoints(endpoints []*pkg.Endpoint) ([]*route53.ResourceRecordSet, error) {
+	var rset []*route53.ResourceRecordSet
+	for _, ep := range endpoints {
+		aliasZoneID := "test"
+		rset = append(rset, mapEndpointAlias(ep, int64(c.Options.RecordSetTTL), &aliasZoneID))
+		rset = append(rset, mapEndpointTXT(ep, int64(c.Options.RecordSetTTL), c.Options.GroupID))
+	}
+	return rset, nil
+}
+
+func (c *Client) ChangeRecordSets(upsert, del []*route53.ResourceRecordSet) error {
 	if err := c.checkFailNext(); err != nil {
 		return err
 	}
 
 	c.LastDelete = del
 	for _, ep := range del {
-		if ep.IP != "" {
-			delete(c.Records, ep.DNSName)
+		if ep.AliasTarget == nil {
+			delete(c.Records, *ep.Name)
 		} else {
-			delete(c.AliasRecords, ep.DNSName)
+			delete(c.AliasRecords, *ep.Name)
 		}
 	}
 
 	c.LastUpsert = upsert
 	for _, ep := range upsert {
-		if ep.IP != "" {
+		if ep.AliasTarget == nil {
 			if c.Records == nil {
 				c.Records = make(map[string]string)
 			}
 
-			c.Records[ep.DNSName] = ep.IP
+			c.Records[*ep.Name] = *ep.Name //need to fix this
 		} else {
 			if c.AliasRecords == nil {
 				c.AliasRecords = make(map[string]string)
 			}
 
-			c.AliasRecords[ep.DNSName] = ep.Hostname
+			c.AliasRecords[*ep.Name] = *ep.AliasTarget.DNSName
 		}
 	}
 
@@ -73,4 +94,52 @@ func (c *Client) FailNext() {
 func (c *Client) checkFailNext() (err error) {
 	err, c.failNext = c.failNext, nil
 	return
+}
+
+func mapEndpointAlias(ep *pkg.Endpoint, ttl int64, aliasHostedZoneID *string) *route53.ResourceRecordSet {
+	rs := &route53.ResourceRecordSet{
+		Type: aws.String("A"),
+		Name: aws.String(pkg.SanitizeDNSName(ep.DNSName)),
+		AliasTarget: &route53.AliasTarget{
+			DNSName:              aws.String(ep.Hostname),
+			EvaluateTargetHealth: aws.Bool(true),
+			HostedZoneId:         aliasHostedZoneID,
+		},
+	}
+	return rs
+}
+
+//mapEndpointTXT ...
+//create an AWS TXT record
+func mapEndpointTXT(ep *pkg.Endpoint, ttl int64, awsGroupID string) *route53.ResourceRecordSet {
+	rs := &route53.ResourceRecordSet{
+		Type: aws.String("TXT"),
+		Name: aws.String(pkg.SanitizeDNSName(ep.DNSName)),
+		TTL:  &ttl,
+		ResourceRecords: []*route53.ResourceRecord{{
+			Value: aws.String(getTXTValue(awsGroupID)),
+		}},
+	}
+	return rs
+}
+
+func getTXTValue(groupID string) string {
+	return fmt.Sprintf("\"mate:%s\"", groupID)
+}
+
+func (c *Client) Diff(rset1 []*route53.ResourceRecordSet, rset2 []*route53.ResourceRecordSet) []*route53.ResourceRecordSet {
+	var diff []*route53.ResourceRecordSet
+	for _, r1 := range rset1 {
+		exist := false
+		for _, r2 := range rset2 {
+			if aws.StringValue(r1.Name) == aws.StringValue(r2.Name) {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			diff = append(diff, r1)
+		}
+	}
+	return diff
 }
