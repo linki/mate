01bc3dd393b4ff1ee939c2f8db921895f7664113
Martin Linkhorst <martin.linkhorst@zalando.de>
2016-12-09 16:42:39 +0100

    feat: allow mate to deploy itself

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+
 	log "github.com/Sirupsen/logrus"
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/zalando-incubator/mate/consumers"
 	"github.com/zalando-incubator/mate/controller"
+	"github.com/zalando-incubator/mate/deploy"
 	"github.com/zalando-incubator/mate/producers"
 )
 
@@ -14,6 +17,7 @@ var params struct {
 	consumer string
 	debug    bool
 	syncOnly bool
+	deploy   bool
 }
 
 var version = "Unknown"
@@ -23,6 +27,7 @@ func init() {
 	kingpin.Flag("consumer", "The endpoints consumer to use.").Required().StringVar(&params.consumer)
 	kingpin.Flag("debug", "Enable debug logging.").BoolVar(&params.debug)
 	kingpin.Flag("sync-only", "Disable event watcher").BoolVar(&params.syncOnly)
+	kingpin.Flag("deploy", "When set this deploys mate in the targt cluster.").BoolVar(&params.deploy)
 }
 
 func main() {
@@ -31,6 +36,15 @@ func main() {
 
 	if params.debug {
 		log.SetLevel(log.DebugLevel)
+	}
+
+	if params.deploy {
+		err := deploy.NewManifest(version, os.Args[1:]).Deploy()
+		if err != nil {
+			log.Fatalf("Error deploying manifest: %v", err)
+		}
+
+		os.Exit(0)
 	}
 
 	p, err := producers.New(params.producer)
