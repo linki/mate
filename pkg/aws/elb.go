d390d3ab3e27fcbaf98cdc9c8590ec2c1fdf5b88
ideahitme <yerken.tussupbekov@zalando.de>
2016-12-28 15:25:49 +0100

    pagination for lbs, concurrent lbs

--- pkg/aws/elb.go
+++ pkg/aws/elb.go
@@ -1,6 +1,9 @@
 package aws
 
 import (
+	"sync"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elb"
@@ -13,6 +16,9 @@ type LoadBalancer struct {
 	DNSName         string
 	CanonicalZoneID string
 }
+
+//LoadBalancerExtractor is a func type which is extract Classic and Application Loadbalancer
+type LoadBalancerExtractor func(*session.Session) ([]*LoadBalancer, error)
 
 //getCanonicalZoneIDs returns the map of LB (ALB + ELB classic) mapped to its CanonicalHostedZoneId
 func (c *Client) getCanonicalZoneIDs(endpoints []*pkg.Endpoint) (map[string]string, error) {
@@ -27,17 +33,28 @@ func (c *Client) getCanonicalZoneIDs(endpoints []*pkg.Endpoint) (map[string]stri
 		return nil, err
 	}
 
-	albs, err := c.getALBs(session)
-	if err != nil {
-		return nil, err
+	loadBalancers := make([]*LoadBalancer, 0)
+
+	var addLBMutex sync.Mutex
+	var wg sync.WaitGroup
+
+	for _, extractor := range []LoadBalancerExtractor{c.getALBs, c.getELBs} {
+		wg.Add(1)
+		go func(extractor LoadBalancerExtractor) {
+			defer wg.Done()
+			lbs, err := extractor(session)
+			if err != nil {
+				log.Errorf("Error getting LBs: %v. Skipping...", err)
+				return
+			}
+			addLBMutex.Lock()
+			loadBalancers = append(loadBalancers, lbs...)
+			addLBMutex.Unlock()
+		}(extractor)
 	}
 
-	elbs, err := c.getELBs(session)
-	if err != nil {
-		return nil, err
-	}
+	wg.Wait()
 
-	loadBalancers := append(albs, elbs...)
 	loadBalancersMap := map[string]string{} //map LB Dns to its canonical hosted zone id
 
 	for _, endpoint := range endpoints {
@@ -52,39 +69,74 @@ func (c *Client) getCanonicalZoneIDs(endpoints []*pkg.Endpoint) (map[string]stri
 }
 
 func (c *Client) getELBs(session *session.Session) ([]*LoadBalancer, error) {
+	result := make([]*LoadBalancer, 0)
+
 	client := elb.New(session)
 
-	resp, err := client.DescribeLoadBalancers(nil)
-	if err != nil {
-		return nil, err
+	params := &elb.DescribeLoadBalancersInput{
+		Marker: nil,
 	}
 
-	loadBalancers := resp.LoadBalancerDescriptions
-	result := make([]*LoadBalancer, len(loadBalancers))
-	for i, loadbalancer := range loadBalancers {
-		result[i] = &LoadBalancer{
-			DNSName:         aws.StringValue(loadbalancer.DNSName),
-			CanonicalZoneID: aws.StringValue(loadbalancer.CanonicalHostedZoneNameID),
+	for {
+		resp, err := client.DescribeLoadBalancers(params)
+		if err != nil {
+			return nil, err
 		}
+		if resp == nil {
+			return nil, ErrInvalidAWSResponse
+		}
+
+		loadBalancers := resp.LoadBalancerDescriptions
+		log.Debugf("Page of elbs. Size: %d. Next marker: %v", len(loadBalancers), aws.StringValue(resp.NextMarker))
+		for _, loadbalancer := range loadBalancers {
+			result = append(result, &LoadBalancer{
+				DNSName:         aws.StringValue(loadbalancer.DNSName),
+				CanonicalZoneID: aws.StringValue(loadbalancer.CanonicalHostedZoneNameID),
+			})
+		}
+
+		if aws.StringValue(resp.NextMarker) == "" {
+			break
+		}
+		params.SetMarker(aws.StringValue(resp.NextMarker))
 	}
+
 	return result, nil
 }
 
 func (c *Client) getALBs(session *session.Session) ([]*LoadBalancer, error) {
+	result := make([]*LoadBalancer, 0)
+
 	client := elbv2.New(session)
 
-	resp, err := client.DescribeLoadBalancers(nil)
-	if err != nil {
-		return nil, err
+	params := &elbv2.DescribeLoadBalancersInput{
+		Marker: nil,
 	}
 
-	loadBalancers := resp.LoadBalancers
-	result := make([]*LoadBalancer, len(loadBalancers))
-	for i, loadbalancer := range loadBalancers {
-		result[i] = &LoadBalancer{
-			DNSName:         aws.StringValue(loadbalancer.DNSName),
-			CanonicalZoneID: aws.StringValue(loadbalancer.CanonicalHostedZoneId),
+	for {
+		resp, err := client.DescribeLoadBalancers(params)
+		if err != nil {
+			return nil, err
 		}
+		if resp == nil {
+			return nil, ErrInvalidAWSResponse
+		}
+
+		loadBalancers := resp.LoadBalancers
+		log.Debugf("Page of albs. Size: %d. Next marker: %v", len(loadBalancers), aws.StringValue(resp.NextMarker))
+		for _, loadbalancer := range loadBalancers {
+			result = append(result, &LoadBalancer{
+				DNSName:         aws.StringValue(loadbalancer.DNSName),
+				CanonicalZoneID: aws.StringValue(loadbalancer.CanonicalHostedZoneId),
+			})
+		}
+
+		if aws.StringValue(resp.NextMarker) == "" {
+			break
+		}
+		params.SetMarker(aws.StringValue(resp.NextMarker))
 	}
+
 	return result, nil
+
 }
